99270ea30f9ebe762345614739161f96cd919051
Helen C. Ablaza <284748959+HelenCAblaza@users.noreply.github.com>
2026-05-23 09:07:04 +0000

    Make first tree canopy wider

--- src/BootScene.js
+++ src/BootScene.js
@@ -17,7 +17,7 @@ class BootScene extends Phaser.Scene {
         this.load.image('pond2', 'assets/pond2.png?v=1');
         this.load.image('pond3', 'assets/pond3.png?v=2');
         this.load.image('pond4', 'assets/pond4.png?v=1');
-        this.load.image('tree1', 'assets/tree1.png?v=2');
+        this.load.image('tree1', 'assets/tree1.png?v=3');
         this.load.image('tree2', 'assets/tree2.png?v=6');
         this.load.image('tree3', 'assets/tree3.png?v=2');
         this.load.image('flower', 'assets/flower.png?v=1');
